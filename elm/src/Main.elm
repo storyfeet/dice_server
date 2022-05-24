66612516518c995ed166e502c165a57242060c57
Matthew Stoodley <matt.storyfeet@gmail.com>
2022-05-24 11:04:41 +0100

    now looks like an app a bit

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,15 +1,38 @@
 module Main exposing(main)
 import Html exposing(..)
 import Html.Attributes exposing(..)
+import Browser exposing (element)
+
 
 type alias Model = 
-    {name:String}
+    { login : Login
+    }
 
+
+type alias Auth =
+    { name:String
+    , key:String
+    }
+
+type Login 
+    = Out
+    | In Auth
+
+init : () -> (Model ,Cmd Msg)
+init _ = 
+    ({ login= Out
+    }, Cmd.none)
+
+
+update: Msg -> Model -> (Model ,Cmd Msg)
+update _ md = 
+    (md, Cmd.none)
 
 qInput nam typ = div []
     [ text (nam ++":")
     , input [type_ typ, name nam] []
     ]
+
 
 
 qform : String -> String -> List(Html msg) -> Html msg
@@ -20,16 +43,34 @@ qform name ac content =
         ]
             
             
-
-
-
-view = div [] 
-    [ h1 [] [text "Elm Dice"]
-    , qform "login" "/login" 
+loginForm
+    = qform "login" "/login" 
         [ qInput "name" "text"
         , qInput "pass" "password"
         ]
+
+
+
+view : Model -> Html Msg
+view md = div [] 
+    [ h1 [] [text "Elm Dice"]
+    , case md.login of
+        Out -> loginForm
+        In a -> p [] [text ("welcome " ++ a.name)]
     ]
 
 
-main = view
+type Msg
+    = Happy
+    | Sad
+
+subscriptions : Model -> Sub msg
+subscriptions model =
+  Sub.none
+
+main = Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
